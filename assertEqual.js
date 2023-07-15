92b5b3e60f42e6c4929f01aca1fe48e2f4bee200
jjyh <janejyh@gmail.com>
2023-07-14 20:42:44 -0400

    assert message with emoji

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F923) + "Assertion Passed:" + actual + "===" + expected);
+  } else console.log(String.fromCodePoint(0x1F621) + "Assertion Failed:" + actual + "!==" + expected);
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
