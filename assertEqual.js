5df3ab8648ac311b505c3201f5fff8b668a141e9
jjyh <janejyh@gmail.com>
2023-07-14 20:52:09 -0400

    change to template literal

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(String.fromCodePoint(0x1F923) + "Assertion Passed:" + actual + "===" + expected);
-  } else console.log(String.fromCodePoint(0x1F621) + "Assertion Failed:" + actual + "!==" + expected);
+  } else console.log(String.fromCodePoint(0x1F621) + `Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
