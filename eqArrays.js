2c7582ea17d469e747fea6dd7ae2eb2197393b50
jjyh <janejyh@gmail.com>
2023-07-15 09:55:33 -0400

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F923) + "Assertion Passed:" + actual + "===" + expected);
+  } else console.log(String.fromCodePoint(0x1F621) + `Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(array1, array2) {
+  let matches = 0;
+  if (array1.length === array2.length){
+    for (i=0; i<=array1.length; i++){
+      if (array1[i] === array2[i]){
+      matches ++;
+      }
+    }
+    if (matches < array1.length){
+      return false;
+    } else return true;
+  } else return false;
+}
+
+
+//test
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+//assertEqual(eqArrays([1, 2, 3], [2, 3, 1]), true);
+//eqArrays([1, 2, 3], [1, 2, 3]) // => true
+//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
