84bf080a8b7b40049004a5b800a918b1b83c3fd3
jjyh <janejyh@gmail.com>
2023-08-22 00:37:49 -0400

    flipped eqObject logic to pass added test

--- eqObjects.js
+++ eqObjects.js
@@ -4,22 +4,27 @@ const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
 
 // Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+//Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
 //They have the same number of keys
 //The value for each key in one object is the same as the value for that same key in the other object
-  if((Object.keys(object1).length) === (Object.keys(object2).length)){
-    for (item of Object.keys(object1)){
-      //if key.value = searchVal then return first key
-      if (object1[item] === object2[item]){
-        return true;
-      }
-      else if (eqArrays(object1[item], object2[item]) === true){
-        return true;
-      } else return false;
-    }
-  } else return false
+  if((Object.keys(object1).length) !== (Object.keys(object2).length)){
+    return false;
+  } else {
+    for (let item of Object.keys(object1)){
+      if ((Array.isArray(object1[item]) && Array.isArray(object2[item]))){
+        if (eqArrays(object1[item], object2[item]) === false){
+          return false;
+        } 
+      } else {
+        if (object1[item] !== object2[item]){
+          return false;
+        }
+      } return true;
+    } 
+  }
 };
+
 
 // TEST CODE (step 3)
 const shirtObject = { color: "red", size: "medium" };
@@ -39,9 +44,9 @@ const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"]
 assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
 
 // re feedback - check if it fails when first item matches but second does not
-const multiColorShirtObject2 = { colors: ["red", "green"], size: "medium" };
-const anotherMultiColorShirtObject2 = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true); // => true
+const AmultiColorShirtObject2 = { colors: ["red", "green"], size: "medium" };
+const AanotherMultiColorShirtObject2 = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(AmultiColorShirtObject2  , AanotherMultiColorShirtObject2), false); // => false
 
 
 module.exports = eqObjects;
